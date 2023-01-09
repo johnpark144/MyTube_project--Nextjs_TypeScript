8a603ede053e3b089bb95692d5d0412818a3c4b1
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-01-09 10:15:18 -0600

    Update Provider.tsx

--- provider/Provider.tsx
+++ provider/Provider.tsx
@@ -9,8 +9,8 @@ function Provider({ children }: ProviderProps) {
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        refetchOnWindowFocus: true,
-        refetchOnMount: false,
+        refetchOnWindowFocus: true, // stale 상태일 경우 윈도우 포커싱 될 때 마다 refetch
+        refetchOnMount: false, //  stale 상태일 경우 마운트 시 마다 refetch
       },
     },
   });
