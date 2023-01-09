7cfa1b8859c27a45bb075664d27b428a14671398
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-01-09 10:21:14 -0600

    Update TimeAgoCreator.tsx

--- app/video/TimeAgoCreator.tsx
+++ app/video/TimeAgoCreator.tsx
@@ -2,7 +2,6 @@ import TimeAgo from "react-timeago";
 
 function TimeAgoCreator({time}:any) {
     const parsedTime = Date.parse(time)
-
   return (
     <>
         <TimeAgo date={new Date(parsedTime)} /> 
