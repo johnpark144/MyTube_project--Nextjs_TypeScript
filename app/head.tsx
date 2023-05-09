f6d4a98a617bbc573c3d7d17d4e1c1e87d7b04ab
Yeonghwan Park (John Park) <106279616+johnpark144@users.noreply.github.com>
2023-05-10 01:19:48 +0900

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,18 @@
 function Head() {
-    return (
-        <>
-          <title>MyYube</title>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-          <link rel="icon" href="https://user-images.githubusercontent.com/106279616/210386840-2818646d-943a-41be-9485-179073f855e0.png" />
-        </>
-      )
+  return (
+    <>
+      <title>MyYube</title>
+      <meta content="yes" name="apple-mobile-web-app-capable" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1 user-scalable=no"
+      />
+      <link
+        rel="icon"
+        href="https://user-images.githubusercontent.com/106279616/210386840-2818646d-943a-41be-9485-179073f855e0.png"
+      />
+    </>
+  );
 }
 
 export default Head;
