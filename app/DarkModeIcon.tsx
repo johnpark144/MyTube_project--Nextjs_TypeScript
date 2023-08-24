cb22c44f06fbe21f1779aa90c687991711ef4f6f
Yeonghwan Park <vyckd354@gmail.com>
2023-08-24 20:11:57 +0900

    Solved UI mix-up for responsive  at NavBar

--- app/DarkModeIcon.tsx
+++ app/DarkModeIcon.tsx
@@ -1,27 +1,29 @@
-import React, { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
-import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import React, { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 
 function DarkModeIcon() {
   const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-// 새로고침시 아이콘 유지
+  // 새로고침시 아이콘 유지
   useEffect(() => {
     setMounted(true);
-  }, []); 
+  }, []);
 
   const renderThemeChanger = () => {
     if (!mounted) return null; // 새로고침시 아이콘 유지
 
-    const currentTheme = theme === "s" ? systemTheme : theme; // systemTheme은 항상 light (일반모드 light 디폴트)
+    const currentTheme = theme === 's' ? systemTheme : theme; // systemTheme은 항상 light (일반모드 light 디폴트)
 
-    if (currentTheme === "dark") {
+    if (currentTheme === 'dark') {
       // 다크모드인경우
       return (
         <button
-          className={'h-12 p-2 mt-2 rounded-xl hover:ring-2 hover:ring-gray-300 bg-gray-200 dark:bg-gray-600'}
-          onClick={() => setTheme("light")}
+          className={
+            'h-10 sm:h-12 p-2 ml-2 rounded-xl hover:ring-2 hover:ring-gray-300 bg-gray-200 dark:bg-gray-600'
+          }
+          onClick={() => setTheme('light')}
         >
           <BsFillSunFill />
         </button>
@@ -30,8 +32,10 @@ function DarkModeIcon() {
       // 일반모드인경우
       return (
         <button
-          className={'h-12 p-2 mt-2 rounded-xl hover:ring-2 hover:ring-gray-300 bg-gray-200'}
-          onClick={() => setTheme("dark")}
+          className={
+            'h-10 sm:h-12 p-2 ml-2 rounded-xl hover:ring-2 hover:ring-gray-300 bg-gray-200'
+          }
+          onClick={() => setTheme('dark')}
         >
           <BsFillMoonFill />
         </button>
@@ -39,11 +43,7 @@ function DarkModeIcon() {
     }
   };
 
-  return (
-    <>
-      {renderThemeChanger()}
-    </>
-  )
+  return <>{renderThemeChanger()}</>;
 }
 
 export default DarkModeIcon;
--- app/Header.tsx
+++ app/Header.tsx
@@ -1,78 +1,95 @@
-"use client";
-import React, { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import Provider from "./../provider/Provider";
-import DarkModeIcon from "./DarkModeIcon";
-import { FcSearch } from "react-icons/fc";
+'use client';
+import { useState, useRef, useEffect } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import Provider from './../provider/Provider';
+import DarkModeIcon from './DarkModeIcon';
+import { FcSearch } from 'react-icons/fc';
 
 function Header() {
   const router = useRouter();
-  const [searchWord, setSearchWord] = useState("");
+  const [searchWord, setSearchWord] = useState('');
+
+  const refOne = useRef<HTMLInputElement>(null);
+  const [seeMyTubeLogo, setSeeMyTubeLogo] = useState(true);
+
+  // refOne의 바깥 돔을 클릭한경우 실행
+  const hideOnClickOutside = (e: Event) => {
+    if (refOne.current && !refOne.current.contains(e.target as Node)) {
+      setSeeMyTubeLogo(true);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener('click', hideOnClickOutside, true);
+  }, []);
 
   // 검색
   const doSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     router.push(`/video/search?q=${searchWord}`);
-    setSearchWord("");
+    setSearchWord('');
   };
 
   return (
     <>
-      <header className="z-40 h-16 w-full items-center rounded-2xl bg-white shadow-lg dark:bg-gray-700">
-        <div className="flex-center relative z-20 flex h-full flex-col justify-center px-1 sm:px-3">
-          <div className="text-lg sm:text-2xl left-0 z-50 flex h-full justify-between">
-            <Link href="/" className="flex">
-              {/* 로고와 MyTube */}
-              <div className="relative top-2 w-[5rem] h-[3rem] sm:top-0 sm:w-24 sm:h-14">
-                <Image
-                  src="https://user-images.githubusercontent.com/106279616/209161034-33287953-5b8f-40b3-ac12-a63010849fbf.png"
-                  fill
-                  alt="MyTube-Logo"
-                />
-              </div>
-              <div className="mt-4 mx-3">MyTube</div>
-            </Link>
-            {/* 검색 */}
-            <form
-              onSubmit={doSearch}
-              className="group absolute top-0 left-[170px] sm:left-[199px] sm:ml-2 lg:ml-0 flex items-center h-full w-[210px] sm:w-[277px]
-              md:w-[479px] lg:relative lg:left-0 lg:w-1/2"
-            >
-              <input
-                type="text"
-                className="aa-input block w-full rounded-2xl bg-gray-100 
+      <header className='z-40 h-16 w-full rounded-xl bg-white shadow-md dark:bg-gray-700'>
+        <div className='text-lg pl-2 sm:px-4 py-2 sm:text-2xl flex h-full justify-between items-center'>
+          <Link
+            href='/'
+            className={`mx-1 sm:mx-2 items-center ${
+              seeMyTubeLogo ? 'flex' : 'hidden sm:flex'
+            }`}
+          >
+            {/* 로고와 MyTube */}
+            <div className='relative w-[5rem] h-[3rem] mr-2 sm:w-24 sm:h-14'>
+              <Image
+                src='https://user-images.githubusercontent.com/106279616/209161034-33287953-5b8f-40b3-ac12-a63010849fbf.png'
+                fill
+                alt='MyTube-Logo'
+              />
+            </div>
+            <div>MyTube</div>
+          </Link>
+          {/* 검색 */}
+          <form
+            onSubmit={doSearch}
+            className='flex-1 flex items-center relative left-6 sm:left-0'
+          >
+            <input
+              type='text'
+              className='w-full rounded-2xl bg-gray-100 
                 py-1.5 pl-10 pr-4 leading-normal text-gray-400 ring-opacity-90 
                 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500
-                 dark:bg-gray-800"
-                placeholder="Search..."
-                onChange={(e) => setSearchWord(e.target.value)}
-                value={searchWord}
-              />
-              <button
-                type="submit"
-                className="relative right-12 w-10 h-10 p-1 rounded-full bg-white"
-              >
-                <FcSearch className="text-3xl" />
-              </button>
-            </form>
+                 dark:bg-gray-800'
+              placeholder='Search...'
+              onChange={(e) => setSearchWord(e.target.value)}
+              onFocus={() => setSeeMyTubeLogo(false)}
+              value={searchWord}
+              ref={refOne}
+            />
+            <button
+              type='submit'
+              className='w-8 h-8 sm:w-10 sm:h-10 p-1 rounded-full bg-white relative -left-10'
+            >
+              <FcSearch className='text-2xl sm:text-3xl' />
+            </button>
+          </form>
+          <div className='flex lg:w-1/4 lg:min-w-[430px] justify-around items-center gap-x-7'>
             {/* 다크모드 */}
-            <div className="absolute right-1 sm:right-12 lg:relative lg:top-0 lg:left-0">
-              <Provider>
-                <DarkModeIcon />
-              </Provider>
-            </div>
+            <Provider>
+              <DarkModeIcon />
+            </Provider>
             {/* 메뉴, Lg사이즈일때만 hidden */}
-            <ol className="hidden lg:flex flex-none justify-evenly text-center lg:flex-row lg:mt-4 ">
-              <Link href="/video">Video</Link>
-              <Link className="ml-8" href="/watchlater">
-                Watch later
-              </Link>
-              <Link className="ml-8" href="/history">
-                History
-              </Link>
-            </ol>
+            <Link className='hidden lg:flex' href='/video'>
+              Video
+            </Link>
+            <Link className='hidden lg:flex' href='/watchlater'>
+              Watch later
+            </Link>
+            <Link className='hidden lg:flex' href='/history'>
+              History
+            </Link>
           </div>
         </div>
       </header>
