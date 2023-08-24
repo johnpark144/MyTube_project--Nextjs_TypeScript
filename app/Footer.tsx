84d5a29c80fa9043487fe513fe2ad541f80875e6
Yeonghwan Park <vyckd354@gmail.com>
2023-08-24 20:31:18 +0900

    Corrected footer's UI

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,36 +1,40 @@
 // 화면이 작아질때만 등장
-import Link from "next/link";
-import { BsCameraVideo, BsSave } from "react-icons/bs";
-import { BiHistory } from "react-icons/bi";
+import Link from 'next/link';
+import { BsCameraVideo, BsSave } from 'react-icons/bs';
+import { BiHistory } from 'react-icons/bi';
 
 function Footer() {
   return (
     <>
       <footer
-        className="visible fixed bottom-0 left-0 z-40 h-16 w-full
+        className='flex fixed bottom-0 left-0 z-40 h-16 w-full
       border-t-2 border-gray-100 border-solid
-      items-center rounded-2xl bg-white dark:bg-gray-700 lg:invisible"
+      rounded-2xl bg-white dark:bg-gray-700 dark:border-gray-800 lg:hidden'
       >
-        <ol className="flex justify-evenly text-center mt-4">
-          <Link className="w-1/4" href="/video">
-            <h1 className="relative left-1/3 px-4">
-              <BsCameraVideo />
-            </h1>
+        <Link className='flex-1 flex justify-center items-center' href='/video'>
+          <div className='w-4/5 flex flex-col items-center'>
+            <BsCameraVideo />
             <h1>Video</h1>
-          </Link>
-          <Link className="w-1/4" href="/watchlater">
-            <h1 className="relative left-1/3 px-4">
-              <BsSave />
-            </h1>
+          </div>
+        </Link>
+        <Link
+          className='flex-1 flex justify-center items-center '
+          href='/watchlater'
+        >
+          <div className='w-4/5 flex flex-col items-center'>
+            <BsSave />
             <h1>Watch later</h1>
-          </Link>
-          <Link className="w-1/4" href="/history">
-            <h1 className="relative left-1/3 px-4">
-              <BiHistory />
-            </h1>
+          </div>
+        </Link>
+        <Link
+          className='flex-1 flex justify-center items-center '
+          href='/history'
+        >
+          <div className='w-4/5 flex flex-col items-center'>
+            <BiHistory />
             <h1>History</h1>
-          </Link>
-        </ol>
+          </div>
+        </Link>
       </footer>
     </>
   );
